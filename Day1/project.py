04f5acb4970d69c0cd0051dddd5d239c0090178d
Danish Raina <danishraina768@gmail.com>
2024-08-08 13:27:03 +0100

    Added Day One Code as well

---
+++ Day1/project.py
@@ -0,0 +1,6 @@
+print("Welcome to Band Name Generator.\n")
+
+city = input("Enter the city of your birth: ")
+pet = input("Enter the name of your pet: ")
+
+print(f'\nYour band could be named: {city} {pet}.')
